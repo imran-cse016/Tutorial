237a6915143a47d7bef062978bbd524ed7a69709
ERP <erp@SDSBLR.COM>
2019-10-31 20:04:06 +0530

    Add project files.

---
+++ Tutorial.Client/Pages/Counter.razor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tutorial.Client.Pages
+{
+    //[Route("/counter")]
+    public partial class Counter
+    {
+
+        int currentCount = 0;
+        void IncrementCount()
+        {
+            currentCount++;
+            Console.WriteLine("Writing to javascript console using C#");
+        }
+
+        void DecrementCount()
+        {
+            currentCount = currentCount - 2;
+            Console.WriteLine($"Counter {currentCount}");
+        }
+
+        [Inject]
+        protected NavigationManager LocalNavigationManagerByCodeBehind { get; set; }
+
+        void NavigationByCodeBehind()
+        {
+            LocalNavigationManagerByCodeBehind.NavigateTo("/component");
+        }
+
+        
+    }
+}
